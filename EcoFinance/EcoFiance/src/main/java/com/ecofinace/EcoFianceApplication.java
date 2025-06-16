28ae9bc3074454e2f35e454b915cb6116b5a9a94
Nabid34_ <nabidhassan34@gmail.com>
2025-06-16 16:54:53 +0200

    Maven instalado

---
+++ EcoFinance/EcoFiance/src/main/java/com/ecofinace/EcoFianceApplication.java
@@ -0,0 +1,13 @@
+package com.ecofinace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoFianceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoFianceApplication.class, args);
+    }
+
+}
---
+++ EcoFinance/EcoFiance/src/test/java/com/ecofinace/EcoFianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecofinace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoFianceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
